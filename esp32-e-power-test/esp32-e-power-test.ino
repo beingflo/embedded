e3c7af0c5e59bf07cb43303a961fae97c5f86491
Florian Marending <florian@marending.dev>
2023-02-27 22:35:30 +0100

    esp now recv / send

--- esp32-e-power-test/esp32-e-power-test.ino
+++ esp32-e-power-test/esp32-e-power-test.ino
@@ -1,9 +1,23 @@
-#include "Arduino.h"
+#include <esp_now.h>
+#include <WiFi.h>
+
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
+{
+  Serial.println(*incomingData);
+}
 
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+
+  if (esp_now_init() != ESP_OK)
+  {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_register_recv_cb(OnDataRecv);
 }
 
 void loop()
--- esp32-power-test/esp32-power-test.ino
+++ esp32-power-test/esp32-power-test.ino
@@ -1,11 +1,57 @@
-#include "Arduino.h"
+#include <esp_now.h>
+#include <WiFi.h>
+
+uint8_t receiver[] = {0x08, 0x3A, 0xF2, 0x14, 0x2A, 0x88};
+
+esp_now_peer_info_t peerInfo;
+
+int counter = 0;
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
 
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+
+  if (esp_now_init() != ESP_OK)
+  {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_register_send_cb(OnDataSent);
+
+  memcpy(peerInfo.peer_addr, receiver, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  if (esp_now_add_peer(&peerInfo) != ESP_OK)
+  {
+    Serial.println("Failed to add peer");
+    return;
+  }
 }
 
 void loop()
 {
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(receiver, (uint8_t *)&counter, sizeof(counter));
+
+  Serial.printf("Trying to print %d\n", counter);
+
+  if (result == ESP_OK)
+  {
+    Serial.println("Sent with success");
+  }
+  else
+  {
+    Serial.println("Error sending the data");
+  }
+  counter++;
+  delay(2000);
 }
