204cf8f61e62c21b8a03fa4b7a3039b3b8294d99
Florian Marending <florian@marending.dev>
2023-03-08 20:26:02 +0100

    Fix blink test

--- power-tests/m5stamp/blink/blink.ino
+++ power-tests/m5stamp/blink/blink.ino
@@ -1,23 +1,12 @@
 void setup()
 {
-  pinMode(A0, OUTPUT);
-  pinMode(A1, OUTPUT);
-  pinMode(A2, OUTPUT);
-  pinMode(A3, OUTPUT);
-  pinMode(A4, OUTPUT);
   pinMode(A5, OUTPUT);
-  pinMode(TX, OUTPUT);
-  pinMode(RX, OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(A0, HIGH);
-  digitalWrite(A1, HIGH);
-  digitalWrite(A2, HIGH);
-  digitalWrite(A3, HIGH);
-  digitalWrite(A4, HIGH);
   digitalWrite(A5, HIGH);
-  digitalWrite(TX, HIGH);
-  digitalWrite(RX, HIGH);
+  delay(1000);
+  digitalWrite(A5, LOW);
+  delay(1000);
 }
