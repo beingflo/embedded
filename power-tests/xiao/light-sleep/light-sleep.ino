2e9292d9ef178ff5802b5aeb7ec74292a689ea9f
Florian Marending <florian@marending.dev>
2023-03-08 19:56:09 +0100

    Fix light sleep example

--- power-tests/xiao/light-sleep/light-sleep.ino
+++ power-tests/xiao/light-sleep/light-sleep.ino
@@ -1,16 +1,20 @@
 #include <Arduino.h>
 
-void light_sleep_setup()
+int led = D10;
+
+void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(led, OUTPUT);
 }
 
-void light_sleep_loop()
+void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(led, HIGH);
+  gpio_hold_en(GPIO_NUM_10);
   esp_sleep_enable_timer_wakeup(10000000);
   esp_light_sleep_start();
-  digitalWrite(LED_BUILTIN, LOW);
+  gpio_hold_dis(GPIO_NUM_10);
+  digitalWrite(led, LOW);
   esp_sleep_enable_timer_wakeup(10000000);
   esp_light_sleep_start();
 }
