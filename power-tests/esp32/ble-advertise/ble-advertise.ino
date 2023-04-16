c687c20ece0c9f9abeb0cd02856caec726b15f7e
Florian Marending <florian@marending.dev>
2023-04-16 21:50:59 +0200

    Complete ble PoC

--- power-tests/esp32/ble-advertise/ble-advertise.ino
+++ power-tests/esp32/ble-advertise/ble-advertise.ino
@@ -1,37 +1,72 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include <BLE2902.h>
 
 #define SERVICE_UUID "3fff1c98-3055-45b2-838e-b384b6fc1125"
 #define CHARACTERISTIC_UUID "a36e7c55-6bd4-4c3c-a003-8fe819de260a"
+
+#define bleServerName "ESP32-TEST-BLE"
+
+bool deviceConnected = false;
+
+unsigned long lastTime = 0;
+
+int counter = 0;
+
+BLECharacteristic characteristic(CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_NOTIFY);
+BLEDescriptor descriptor(BLEUUID((uint16_t)0x2902));
+
+// Setup callbacks onConnect and onDisconnect
+class MyServerCallbacks : public BLEServerCallbacks
+{
+  void onConnect(BLEServer *pServer)
+  {
+    deviceConnected = true;
+  };
+  void onDisconnect(BLEServer *pServer)
+  {
+    deviceConnected = false;
+  }
+};
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println("Starting BLE");
 
-  BLEDevice::init("ble-test");
-  BLEServer *pServer = BLEDevice::createServer();
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
-      CHARACTERISTIC_UUID,
-      BLECharacteristic::PROPERTY_READ |
-          BLECharacteristic::PROPERTY_WRITE);
+  BLEDevice::init(bleServerName);
 
-  pCharacteristic->setValue("hello world");
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  pService->addCharacteristic(&characteristic);
+  descriptor.setValue("Counter");
+  characteristic.addDescriptor(&descriptor);
+  // Start the service
   pService->start();
+
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
-  pAdvertising->setScanResponse(true);
-  pAdvertising->setMinPreferred(0x06);
-  pAdvertising->setMinPreferred(0x12);
-
-  BLEDevice::startAdvertising();
-
-  Serial.println("Done");
+  pServer->getAdvertising()->start();
+  Serial.println("Waiting a client connection to notify...");
 }
 
 void loop()
 {
-  delay(2000);
+  if (deviceConnected)
+  {
+    if ((millis() - lastTime) > 5000)
+    {
+      char data[4];
+      dtostrf(counter, 4, 2, data);
+      characteristic.setValue(data);
+      characteristic.notify();
+
+      lastTime = millis();
+      counter += 1;
+    }
+  }
 }
--- power-tests/esp32/ble-scan/ble-scan.ino
+++ power-tests/esp32/ble-scan/ble-scan.ino
@@ -1,38 +1,108 @@
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEScan.h>
-#include <BLEAdvertisedDevice.h>
+#include "BLEDevice.h"
 
-int scanTime = 5; // In seconds
-BLEScan *pBLEScan;
+#define bleServerName "ESP32-TEST-BLE"
+
+static BLEUUID serviceUUID("3fff1c98-3055-45b2-838e-b384b6fc1125");
+static BLEUUID characteristicUUID("a36e7c55-6bd4-4c3c-a003-8fe819de260a");
+
+static boolean doConnect = false;
+static boolean connected = false;
+
+static BLEAddress *pServerAddress;
+static BLERemoteCharacteristic *characteristic;
+
+const uint8_t notificationOn[] = {0x1, 0x0};
+const uint8_t notificationOff[] = {0x0, 0x0};
+
+char *counter;
+
+boolean newCounter = false;
+
+// Connect to the BLE Server that has the name, Service, and Characteristics
+bool connectToServer(BLEAddress pAddress)
+{
+  BLEClient *pClient = BLEDevice::createClient();
+
+  pClient->connect(pAddress);
+  Serial.println("Connected to server");
+
+  BLERemoteService *pRemoteService = pClient->getService(serviceUUID);
+  if (pRemoteService == nullptr)
+  {
+    Serial.print("Failed to find our service UUID: ");
+    Serial.println(serviceUUID.toString().c_str());
+    return (false);
+  }
+
+  characteristic = pRemoteService->getCharacteristic(characteristicUUID);
+
+  if (characteristic == nullptr)
+  {
+    Serial.print("Failed to find our characteristic UUID");
+    return false;
+  }
+  Serial.println(" - Found our characteristics");
+
+  // Assign callback functions for the Characteristics
+  characteristic->registerForNotify(counterNotifyCallback);
+  return true;
+}
 
 class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
 {
   void onResult(BLEAdvertisedDevice advertisedDevice)
   {
-    Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
+    if (advertisedDevice.getName() == bleServerName)
+    {
+      advertisedDevice.getScan()->stop();
+      pServerAddress = new BLEAddress(advertisedDevice.getAddress());
+      doConnect = true;
+      Serial.println("Device found. Connecting!");
+    }
   }
 };
+
+static void counterNotifyCallback(BLERemoteCharacteristic *pBLERemoteCharacteristic,
+                                  uint8_t *pData, size_t length, bool isNotify)
+{
+  counter = (char *)pData;
+  newCounter = true;
+}
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("Scanning...");
+  Serial.println("Starting ble client");
 
   BLEDevice::init("");
-  pBLEScan = BLEDevice::getScan();
+
+  BLEScan *pBLEScan = BLEDevice::getScan();
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
   pBLEScan->setActiveScan(true);
-  pBLEScan->setInterval(100);
-  pBLEScan->setWindow(99);
+  pBLEScan->start(30);
 }
 
 void loop()
 {
-  BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
-  Serial.print("Devices found: ");
-  Serial.println(foundDevices.getCount());
-  Serial.println("Scan done!");
-  pBLEScan->clearResults();
-  delay(2000);
+  if (doConnect == true)
+  {
+    if (connectToServer(*pServerAddress))
+    {
+      Serial.println("Connected to server");
+      // Activate the Notify property of each Characteristic
+      characteristic->getDescriptor(BLEUUID((uint16_t)0x2902))->writeValue((uint8_t *)notificationOn, 2, true);
+      connected = true;
+    }
+    else
+    {
+      Serial.println("Failed to connect to server");
+    }
+    doConnect = false;
+  }
+  if (newCounter)
+  {
+    newCounter = false;
+    Serial.println(counter);
+  }
+  delay(1000);
 }
