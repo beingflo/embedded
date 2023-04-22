398f6cb1455519baafb4d780039f12a5ab269ce7
Florian Marending <florian@marending.dev>
2023-04-22 21:09:34 +0200

    Add light-sensor code

---
+++ light-sensor/light-sensor.ino
@@ -0,0 +1,124 @@
+#include <Wire.h>
+#include <BH1750.h>
+#include <esp_now.h>
+#include <WiFi.h>
+#include <esp_wifi.h>
+
+uint8_t receiver[] = {0x24, 0x4C, 0xAB, 0x82, 0xF5, 0x0C};
+
+esp_now_peer_info_t peerInfo;
+
+BH1750 lightMeter;
+int success = 0;
+
+struct msg_data
+{
+  float lux;
+  int num_tries;
+};
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
+  if (status == ESP_NOW_SEND_SUCCESS)
+  {
+    success = 1;
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.setTxTimeoutMs(0);
+
+  WiFi.mode(WIFI_STA);
+
+  Wire.begin();
+
+  lightMeter.begin(BH1750::ONE_TIME_HIGH_RES_MODE);
+
+  if (esp_now_init() != ESP_OK)
+  {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  if (esp_now_register_send_cb(OnDataSent) != ESP_OK)
+  {
+    Serial.println("Error registering callback");
+    return;
+  }
+
+  memcpy(peerInfo.peer_addr, receiver, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  if (esp_now_add_peer(&peerInfo) != ESP_OK)
+  {
+    Serial.println("Error adding peer");
+    return;
+  }
+}
+
+void loop()
+{
+  esp_wifi_start();
+
+  while (!lightMeter.measurementReady(true))
+  {
+    yield();
+  }
+
+  float lux = lightMeter.readLightLevel();
+  Serial.print("Light: ");
+  Serial.print(lux);
+  Serial.println(" lx");
+
+  lightMeter.configure(BH1750::ONE_TIME_HIGH_RES_MODE);
+
+  int num_tries = 0;
+
+  // Resend until data was received by recipient
+  while (!success && num_tries < 5)
+  {
+    esp_err_t result = ESP_FAIL;
+
+    num_tries += 1;
+
+    int send_tries = 0;
+    // Try sending until successfully sent
+    while (result != ESP_OK && send_tries < 20)
+    {
+      send_tries += 1;
+
+      struct msg_data data = {lux, num_tries};
+
+      result = esp_now_send(receiver, (uint8_t *)&data, sizeof(msg_data));
+
+      if (result != ESP_OK)
+      {
+        Serial.println("Error sending the data");
+        break;
+      }
+    }
+
+    if (send_tries == 20)
+    {
+      Serial.println("ESP can't send");
+    }
+
+    // Allow time for callback to signal success
+    delay(100);
+  }
+
+  if (num_tries == 5)
+  {
+    Serial.println("Retries exhausted");
+  }
+
+  success = 0;
+
+  esp_wifi_stop();
+  esp_sleep_enable_timer_wakeup(1);
+
+  esp_light_sleep_start();
+}
