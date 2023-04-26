36d43826a38e8e131bde9be7652ebeac534e53cb
Florian Marending <florian@marending.dev>
2023-04-26 22:18:19 +0200

    Add vBat reporting

--- co2-sensor/co2-sensor.ino
+++ co2-sensor/co2-sensor.ino
@@ -23,17 +23,18 @@ struct msg_data
   float lux;
   float temperature;
   float humidity;
+  float vBat;
   int num_tries;
 };
 
-struct msg_data data = {-5, -5, -5, 0};
+struct msg_data data = {-5, -5, -5, -5, 0};
 
 // callback function that will be executed when data is received
 void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
 {
   memcpy(&data, incomingData, sizeof(msg_data));
 
-  Serial.printf("lux: %f, temperature: %f, humidity: %f, retries: %d\n", data.lux, data.temperature, data.humidity, data.num_tries);
+  Serial.printf("lux: %f, temperature: %f, humidity: %f, vBat: %f, retries: %d\n", data.lux, data.temperature, data.humidity, data.vBat, data.num_tries);
 }
 
 void connectWifi()
@@ -134,10 +135,11 @@ void loop()
       String lux_esp = "#TYPE home_lux gauge\nhome_lux " + String(data.lux) + "\n";
       String temp_esp = "#TYPE home_temp gauge\nhome_temp " + String(data.temperature) + "\n";
       String hum_esp = "#TYPE home_hum gauge\nhome_hum " + String(data.humidity) + "\n";
+      String vBat_esp = "#TYPE home_vBat gauge\nhome_vBat " + String(data.vBat) + "\n";
 
-      data = {-5, -5, -5, 1};
+      data = {-5, -5, -5, -5, 1};
 
-      body = body + lux_esp + temp_esp + hum_esp;
+      body = body + lux_esp + temp_esp + hum_esp + vBat_esp;
     }
 
     http.begin(endpoint.c_str());
