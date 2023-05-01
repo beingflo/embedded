7f8d27547b1d4c8f57933ed8e8b9ed4e30ff78c4
Florian Marending <florian@marending.dev>
2023-05-01 15:49:45 +0200

    Add bmp280 sketch

---
+++ power-tests/xiao/bmp280/bmp280.ino
@@ -0,0 +1,39 @@
+#include <Adafruit_BMP280.h>
+
+Adafruit_BMP280 bmp;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  while (!Serial)
+    delay(100);
+
+  unsigned status = bmp.begin(BMP280_ADDRESS_ALT);
+
+  while (!status)
+  {
+    Serial.println("Not connected");
+    delay(1000);
+  }
+
+  /* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+}
+
+void loop()
+{
+  Serial.print(F("Temperature = "));
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
+
+  Serial.print(F("Pressure = "));
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
+
+  delay(2000);
+}
