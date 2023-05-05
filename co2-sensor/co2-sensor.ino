6bce34c5b147f9b8726a1f72d94960bfa7b3796d
Florian Marending <florian@marending.dev>
2023-05-05 21:13:58 +0200

    Add pressure gateway to co2 sensor

--- co2-sensor/co2-sensor.ino
+++ co2-sensor/co2-sensor.ino
@@ -11,14 +11,16 @@ RTC_DATA_ATTR int counter = 0;
 
 const char *ssid = "";
 const char *password = "";
-String endpoint = "";
+String endpoint_88 = "";
+String endpoint_e0 = "";
+String endpoint_fc = "";
 
 const int CLK = D2;
 const int DIO = D3;
 
 TM1637 tm(CLK, DIO);
 
-struct msg_data
+struct msg_data_e0
 {
   float lux;
   int moisture;
@@ -26,14 +28,30 @@ struct msg_data
   int num_tries;
 };
 
-struct msg_data data = {-5, -5, -5, 0};
+struct msg_data_fc
+{
+  float pressure;
+  float temperature;
+  int num_tries;
+};
+
+struct msg_data_e0 data_e0 = {-5, -5, -5, 0};
+struct msg_data_fc data_fc = {0.0, 0.0, 0};
 
 // callback function that will be executed when data is received
-void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
+void OnDataRecv(const uint8_t *mac_addr, const uint8_t *incomingData, int len)
 {
-  memcpy(&data, incomingData, sizeof(msg_data));
-
-  Serial.printf("lux: %f, moisture: %d, vBat: %f, retries: %d\n", data.lux, data.moisture, data.vBat, data.num_tries);
+  Serial.printf("Received message from %02x\n", mac_addr[5]);
+  if (mac_addr[5] == 224)
+  {
+    memcpy(&data_e0, incomingData, sizeof(msg_data_e0));
+    Serial.printf("lux: %f, moisture: %d, vBat: %f, retries: %d\n", data_e0.lux, data_e0.moisture, data_e0.vBat, data_e0.num_tries);
+  }
+  else if (mac_addr[5] == 252)
+  {
+    memcpy(&data_fc, incomingData, sizeof(msg_data_fc));
+    Serial.printf("pressure: %f, temperature: %f, retries: %d\n", data_fc.pressure, data_fc.temperature, data_fc.num_tries);
+  }
 }
 
 void connectWifi()
@@ -123,24 +141,13 @@ void loop()
   {
     HTTPClient http;
 
-    String co2_prom = "#TYPE rpi_home_co2 gauge\nrpi_home_co2 " + String(co2) + "\n";
-    String hum_prom = "#TYPE rpi_home_humidity gauge\nrpi_home_humidity " + String(hum) + "\n";
-    String temp_prom = "#TYPE rpi_home_temperature gauge\nrpi_home_temperature " + String(temp) + "\n";
+    String co2_prom = "#TYPE home_co2 gauge\nhome_co2 " + String(co2) + "\n";
+    String hum_prom = "#TYPE home_humidity gauge\nhome_humidity " + String(hum) + "\n";
+    String temp_prom = "#TYPE home_temperature gauge\nhome_temperature " + String(temp) + "\n";
 
     String body = co2_prom + hum_prom + temp_prom;
 
-    if (data.lux > -4)
-    {
-      String lux_esp = "#TYPE home_lux gauge\nhome_lux " + String(data.lux) + "\n";
-      String moisture_esp = "#TYPE home_moisture gauge\nhome_moisture " + String(data.moisture) + "\n";
-      String vBat_esp = "#TYPE home_vBat gauge\nhome_vBat " + String(data.vBat) + "\n";
-
-      data = {-5, -5, -5, 1};
-
-      body = body + lux_esp + moisture_esp + vBat_esp;
-    }
-
-    http.begin(endpoint.c_str());
+    http.begin(endpoint_88.c_str());
 
     int responseCode = http.POST(body);
 
@@ -157,6 +164,66 @@ void loop()
 
     http.end();
   }
+
+  if (WiFi.status() == WL_CONNECTED && data_e0.lux > -4)
+  {
+    HTTPClient http;
+
+    String lux_e0 = "#TYPE home_lux gauge\nhome_lux " + String(data_e0.lux) + "\n";
+    String moisture_e0 = "#TYPE home_moisture gauge\nhome_moisture " + String(data_e0.moisture) + "\n";
+    String vBat_e0 = "#TYPE home_e0_vBat gauge\nhome_e0_vBat " + String(data_e0.vBat) + "\n";
+
+    data_e0 = {-5, -5, -5, 1};
+
+    String body = lux_e0 + moisture_e0 + vBat_e0;
+
+    http.begin(endpoint_e0.c_str());
+
+    int responseCode = http.POST(body);
+
+    if (responseCode > 0)
+    {
+      Serial.print("HTTP Response: ");
+      Serial.println(responseCode);
+    }
+    else
+    {
+      Serial.print("Error: ");
+      Serial.println(responseCode);
+    }
+
+    http.end();
+  }
+
+  if (WiFi.status() == WL_CONNECTED && data_fc.pressure > 0)
+  {
+    HTTPClient http;
+
+    String pressure_fc = "#TYPE home_outside_pressure gauge\nhome_outside_pressure " + String(data_fc.pressure) + "\n";
+    String temperature_fc = "#TYPE home_outside_temperature gauge\nhome_outside_temperature " + String(data_fc.temperature) + "\n";
+
+    data_fc = {0, 0, 0};
+
+    String body = pressure_fc + temperature_fc;
+
+    http.begin(endpoint_fc.c_str());
+
+    int responseCode = http.POST(body);
+
+    if (responseCode > 0)
+    {
+      Serial.print("HTTP Response: ");
+      Serial.println(responseCode);
+    }
+    else
+    {
+      Serial.print("Error: ");
+      Serial.println(responseCode);
+    }
+
+    http.end();
+  }
+
   if (WiFi.status() != WL_CONNECTED)
   {
     Serial.println("Wifi disconnected");
@@ -165,5 +232,5 @@ void loop()
 
   counter += 1;
 
-  delay(15000);
+  delay(30000);
 }
